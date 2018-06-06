4248c5a2cd20a3b95ceaff2a31c1dd5943becca1
Saransh <saransh661@gmail.com>
2018-06-06 16:10:11 +0530

    streamline matlab encoding and adding yolo offsets

---
+++ mat_to_json/encoder.m
@@ -0,0 +1,26 @@
+mat = dir('*.mat');
+for q = 1:length(mat)
+    y = load(mat(q).name);
+    labels = y.gTruth.LabelData;
+    for j = 1:length(labels{1,:})
+        for i = 1:length(labels{:,1})
+            a = labels{:,:}{i,j};
+            if ne(length(a),0)
+                
+                a(:,3) = a(:,1)+ a(:,3);
+                y2 = a(:,2)+ a(:,4);
+                a(:,4) = a(:,2);
+                a(:,2) = y2;
+                labels{:,:}{i,j} = a;
+            end
+         end
+    end
+    paths = y.gTruth.DataSource.Source;
+    dict = [paths, labels];
+end
+
+text = jsonencode(dict);
+timestamp = datestr(now,'YY-mm-DD-HH-MM');
+filename = strcat('annotations-',datestr(now,'YY-mm-DD-HH-MM'),'.json');
+fid = fopen(filename, 'w');
+fwrite(fid, text, 'char');
